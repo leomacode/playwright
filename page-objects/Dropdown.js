3f21694cc0b2aeaad4ce3b6fb55a9d9ba2f7940c
Leo M <forza.leoma@gmail.com>
2023-07-26 17:02:10 +0200

    add dropdown

--- page-objects/Dropdown.js
+++ page-objects/Dropdown.js
@@ -1,6 +1,24 @@
-export default class DropDown {
+export default class Dropdown {
     constructor(page) {
         this.page = page
+        this.sortBtn = page.locator('[data-qa="sort-dropdown"]')
 
+    }
+
+
+    async clickSortBtn() {
+        await this.sortBtn.click()
+    }
+
+    async ascend() {
+        await this.sortBtn.selectOption('price-asc');
+        await this.page.waitForFunction((expectation) => {
+            const selectedOption = document.querySelector('[data-qa="sort-dropdown"] option:checked').value
+            return selectedOption === expectation
+        }, 'price-asc')
+        await this.page.pause()
+    }
+    async descend() {
+        await this.sortBtn.selectOption('price-desc');
     }
 }
