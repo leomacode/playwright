da7c4226db40d6acbc4f61f894ae5ff03ffe4a94
Leo M <forza.leoma@gmail.com>
2023-07-27 16:51:11 +0200

    add helper functions

--- page-objects/Dropdown.js
+++ page-objects/Dropdown.js
@@ -1,7 +1,10 @@
+import { expect } from "@playwright/test"
+
 export default class Dropdown {
     constructor(page) {
         this.page = page
         this.sortBtn = page.locator('[data-qa="sort-dropdown"]')
+        this.prices = page.locator('[datatype="product-price"]')
 
     }
 
@@ -10,15 +13,52 @@ export default class Dropdown {
         await this.sortBtn.click()
     }
 
+    numConvert(arr) {
+        return arr.map(str => Number(str.replace('$', '')))
+    }
+
+    isIncline(arr) {
+        const len = arr.lenth
+        for (let i = 1; i < len; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    isDecline(arr) {
+        const len = arr.lenth
+        for (let i = 1; i < len; i++) {
+            if (arr[i] > arr[i - 1]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    async getPriceList() {
+        const priceList = await this.prices.allInnerTexts()
+        return this.numConvert(priceList)
+    }
+
     async ascend() {
         await this.sortBtn.selectOption('price-asc');
         await this.page.waitForFunction((expectation) => {
             const selectedOption = document.querySelector('[data-qa="sort-dropdown"] option:checked').value
             return selectedOption === expectation
         }, 'price-asc')
-        await this.page.pause()
+        const prices = await this.getPriceList()
+
+        expect(this.isIncline(prices)).toBeTruthy()
     }
     async descend() {
         await this.sortBtn.selectOption('price-desc');
+        await this.page.waitForFunction((expectation) => {
+            const selectedOption = document.querySelector('[data-qa="sort-dropdown"] option:checked').value
+            return selectedOption === expectation
+        }, 'price-desc')
+        const prices = await this.getPriceList()
+        expect(this.isDecline(prices)).toBeTruthy()
     }
 }
