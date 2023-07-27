7becc9e160ab3c516c43f787b2958abfc03578ec
Leo M <forza.leoma@gmail.com>
2023-07-27 16:52:57 +0200

    add helper functions

--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -5,18 +5,20 @@ import { ProductsPage, Navigation, Checkout, Dropdown } from "../page-objects"
 test.only('new user full end to end journey', async ({ page }) => {
     const productsPage = new ProductsPage(page)
     await productsPage.visit()
-    // await productsPage.addProductsToBasket(0)
-    // await productsPage.addProductsToBasket(1)
-    // await productsPage.addProductsToBasket(2)
-    // const navigation = new Navigation(page)
-    // await navigation.goToCheckout()
-
-
-    // const checkout = new Checkout(page)
-    // await checkout.removeCheapestProduct()
-
     const dropdown = new Dropdown(page)
     await dropdown.ascend()
     await dropdown.descend()
-    await page.pause()
+
+
+    await productsPage.addProductsToBasket(0)
+    await productsPage.addProductsToBasket(1)
+    await productsPage.addProductsToBasket(2)
+    const navigation = new Navigation(page)
+    await navigation.goToCheckout()
+
+
+    const checkout = new Checkout(page)
+    await checkout.removeCheapestProduct()
+
+
 })
