eb4c9017c718189d8bc2ee82a6b5c4fafbcb8b02
Leo M <forza.leoma@gmail.com>
2023-07-31 07:30:23 +0200

    add input fill function

--- page-objects/PageRegister.js
+++ page-objects/PageRegister.js
@@ -1,5 +1,17 @@
 export default class PageRegister {
     constructor(page) {
         this.page = page
+        this.emailField = page.getByPlaceholder('E-Mail')
+        this.passwordField = page.getByPlaceholder('Password')
+    }
+
+
+    async goToRegister() {
+        const emailField = this.emailField
+        await emailField.waitFor()
+        await emailField.fill(process.env.USER_EMAIL);
+        const passwordField = this.passwordField
+        await passwordField.waitFor()
+        await passwordField.fill(process.env.PASSWORD);
     }
 }
