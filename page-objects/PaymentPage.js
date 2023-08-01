57703e85dcbefdc8180f858179a81beac2d0b36d
Leo M <forza.leoma@gmail.com>
2023-08-01 15:23:08 +0200

    add paymentpage

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -1,9 +1,14 @@
 export default class PaymentPage {
     constructor(page) {
         this.page = page
+        this.discountCode = page.frameLocator('[data-qa="active-discount-container"]').getByText('c87ef911ceeb')
     }
 
-    async activateDiscount() { }
+    async activateDiscount() {
+        const discountCode = this.discountCode
+        const code = await discountCode.innerText()
+        console.log(code);
+    }
 
 
 }
