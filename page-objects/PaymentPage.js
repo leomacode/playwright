3c6f5916fce0d77dbc05ed497a7bf5b5aa75cb56
Leo M <forza.leoma@gmail.com>
2023-08-02 17:07:17 +0200

    add activate discount func

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -1,14 +1,33 @@
+import { expect } from "@playwright/test"
+
 export default class PaymentPage {
     constructor(page) {
         this.page = page
-        this.discountCode = page.frameLocator('[data-qa="active-discount-container"]').getByText('c87ef911ceeb')
+        this.discountCode = page.frameLocator('[data-qa="active-discount-container"]').locator('[data-qa="discount-code"]')
+        this.discountInput = page.getByPlaceholder('Discount code')
+        this.submitDiscountBtn = page.getByRole('button', { name: 'Submit discount' })
+        this.activatedInfo = page.locator('[data-qa="discount-active-message"]')
     }
 
     async activateDiscount() {
         const discountCode = this.discountCode
+        discountCode.waitFor()
         const code = await discountCode.innerText()
-        console.log(code);
+        const discountInput = this.discountInput
+        await discountInput.waitFor()
+
+        await discountInput.focus()
+        await this.page.keyboard.insertText(code);
+        expect(discountInput).toHaveValue(code)
+
+        await this.page.waitForFunction(expec =>
+            document.querySelector('[data-qa="discount-code-input"]').value === expec, code)
+
+        const submitDiscountBtn = this.submitDiscountBtn
+        submitDiscountBtn.waitFor()
+        await submitDiscountBtn.click()
+
+        const activatedInfo = this.activatedInfo
+        expect(activatedInfo).toHaveText('Discount activated!')
     }
-
-
 }
