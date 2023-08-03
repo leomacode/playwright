6131aa949373c1c5439f69fc1f77f01bb472b8db
Leo M <forza.leoma@gmail.com>
2023-08-03 15:39:13 +0200

    updare payment details

--- data/paymentDetails.js
+++ data/paymentDetails.js
@@ -1,6 +1,6 @@
 const paymentDetails = {
-    "card_owner": "LEO",
-    "card_number": "123456",
+    "card_owner": "Leo Amsterdam",
+    "card_number": "1111222233334444",
     "valid_until": "11/31",
     "cvc": "222",
 }
