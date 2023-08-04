45ae3d3d06abaee42efb2aa80bf8dc0b5aac0a81
Leo M <forza.leoma@gmail.com>
2023-08-04 14:04:57 +0200

    create myaccount class

--- page-objects/MyAccount.js
+++ page-objects/MyAccount.js
@@ -5,5 +5,16 @@ export default class MyAccount {
 
     async visit() {
         await this.page.goto('/my-account')
+
+        //make a request to get login token
+        const requestContext = await request.newContext({
+            httpCredentials: {
+                username: 'admin',
+                password: 'Admin123'
+            }
+        });
+
+        await requestContext.get(`/my-account`);
+        //inject the login token into browser 
     }
 }
--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -4,5 +4,6 @@ import { MyAccount } from '../page-objects'
 test.only('my account using cookie injection', async ({ page }) => {
     const myAccount = new MyAccount(page)
     await myAccount.visit()
+
     await page.pause()
 })
